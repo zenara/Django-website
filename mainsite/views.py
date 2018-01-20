7a4e14d98112af01aa3b116c767340ecd2e4f0e0
zenara <zenaronline@gmail.com>
2018-01-21 02:26:30 +0530

    Adding Models, Data Migrations and Debugging

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from datetime import datetime
+from .models import Article
 
 def index(request):
     context = {
@@ -16,8 +17,22 @@ def about(request):
     return render(request, 'about.html',context)
 
 def news(request):
+    populate_db()
+    articles = get_articles()
+
     context = {
+        'articles':articles,
         'current_date':datetime.now(),
         'title':'News'
     }
     return render(request, 'news.html',context)
+
+def get_articles():
+    result = Article.objects.all()
+    return result
+
+def populate_db():
+    if Article.objects.count()==0:
+        Article(title = 'first item', content = 'This is the first  test content').save()
+        Article(title = 'sec item', content = 'This is the second test content').save()
+        Article(title = 'third item', content = 'This is the third test content').save()
