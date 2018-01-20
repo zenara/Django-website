6986a15cdcb409bafb143a8654da62bbd72bcba1
zenara <zenaronline@gmail.com>
2018-01-21 01:36:25 +0530

    Adding About and News Pages

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from .views import index
+from .views import index,about,news
 from django.conf import settings
 
 urlpatterns = [
     url(r'^$', index),
+    url(r'^about/$', about),
+    url(r'^news/$', news),
     url(r'^admin/', admin.site.urls),
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -7,3 +7,17 @@ def index(request):
         'title':'Home'
     }
     return render(request, 'index.html',context)
+
+def about(request):
+    context = {
+        'current_date':datetime.now(),
+        'title':'About'
+    }
+    return render(request, 'about.html',context)
+
+def news(request):
+    context = {
+        'current_date':datetime.now(),
+        'title':'News'
+    }
+    return render(request, 'news.html',context)
